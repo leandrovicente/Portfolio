5001423440a06de75371f2cf67861cc8beec7d42
unknown <leandro@biothanks.com>
2020-12-11 03:09:38 -0300

    start next level

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faGem from '@fortawesome/fontawesome-free-regular/faGem'
+
+const Header = (props) => (
+    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+        <div className="logo">
+            {/* <span className="icon fa-spider"></span> */}
+            <FontAwesomeIcon icon={faGem} transform="grow-18" />
+        </div>
+        <div className="content">
+            <div className="inner">
+                <h1>Leandro Vicente</h1>
+                <p>IN LOVE WITH TECHNOLOGY AND CREATING NEW THINGS..</p>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+Header.propTypes = {
+    onOpenArticle: PropTypes.func,
+    timeout: PropTypes.bool
+}
+
+export default Header
